fc1a3a9f36125684f179602bb192098473a00c9f
BISWAS SEKHAR <165394583+romandimitri@users.noreply.github.com>
2024-04-17 22:42:23 +0530

    Home page completed

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -3,8 +3,14 @@
 import { HoverBorderGradient } from "../components/hover-border-gradient";
 import { BackgroundBeams } from "../components/background-beams";
 import Link from 'next/link'; // Import the Link component
+import { UserButton, auth } from "@clerk/nextjs"; // Combine imports for clarity
 
-export default function Home() {
+export default async function Home() {
+ // Use the useUser hook to get the current user
+ const { user } = await auth();
+ // Determine if the user is authenticated
+ const isAuth = !!user;
+
  return (
     <div className="h-screen">
       <div className="h-full rounded-md bg-neutral-90 relative flex flex-col items-center justify-center antialiased">
@@ -19,8 +25,8 @@ export default function Home() {
               width="500" // Set width as per your requirement
               height="500" // Set height as per your requirement
             />
+            <UserButton afterSignOutUrl="/" />
           </div>
-          <p></p>
           <p className="relative z-10 text-lg md:text-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-regular" style={{ marginTop: '10px', paddingTop: '10px' }}>
             Where KTU Studies Made Easier
           </p>
@@ -35,6 +41,18 @@ export default function Home() {
               <span>Sign In</span>
             </HoverBorderGradient>
           </Link>
+          {isAuth && (
+            <Link href="/signin"> {/* Specify the path to navigate to */}
+              <HoverBorderGradient
+                containerClassName="rounded-full"
+                as="button"
+                className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+                style={{ marginLeft: '10px', paddingLeft: '10px' }}
+              >
+                <span>Go to Chat</span>
+              </HoverBorderGradient>
+            </Link>
+          )}
         </div>
         <BackgroundBeams />
       </div>
